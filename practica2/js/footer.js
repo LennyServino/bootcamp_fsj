9268a758661cd0798d5c233108b2a542dd5edf18
Lenny Servino <lennyservino@gmail.com>
2024-07-22 17:43:16 -0600

    agregando carpetas y archivos de  practica2 y refuerzos

---
+++ practica2/js/footer.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    footerContent();
+});
+
+function footerContent() {
+    const footer = document.getElementById('footer');
+    footer.innerHTML = '<a href="">BlogDeCafé</a>';
+}
---
+++ practica2/js/navbar.js
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    const Header = document.getElementById('header');
+
+    Header.innerHTML = `
+        <section class="nav">
+            <p>Blog DeCafe</p>
+            <nav>
+                <ul>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Cursos</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </nav>
+        </section>
+        <section class="banner">
+            <h1>Blog de cafe de consejos y cursos</h1>
+            <p>Aprende de los expertos con las mejores recetas y consejos</p>
+        </section>
+    `;
+}
+
+Navbar();
