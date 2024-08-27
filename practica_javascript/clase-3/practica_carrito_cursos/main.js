578ef4306bcfd75832c8c42f75177411ead1e17a
Lenny Servino <lennyservino@gmail.com>
2024-08-27 13:41:40 -0600

    Agregando funcion vaciar y calcular valor a practica carrito

--- practica_javascript/clase-3/practica_carrito_cursos/main.js
+++ practica_javascript/clase-3/practica_carrito_cursos/main.js
@@ -2,16 +2,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './public/assets/style.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-import { agregarCurso } from './src/components/carrito';
+import { agregarCurso, eliminarCurso, vaciarCarrito } from './src/components/carrito';
 
 //evento de escucha cuando se recarga la pagina
 document.addEventListener('DOMContentLoaded', () => {
     //llamando el elemento del main
     const listadoCursos = document.querySelector('#lista-cursos');
-
+    //llamando el elemento table
+    const tablaCurso = document.querySelector('#lista-carrito');
+    //llamando al boton vaciar carrito
+    const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
     //alojar todos los eventos para el carrito
     /*
      * Mandamos un evento de escucha al contenido del main, cuando la persona le de click se ejecuta la funcion 'agregarCurso'    
     */
     listadoCursos.addEventListener('click', agregarCurso);
+
+    //evento de escucha para ejecutar eliminar curso
+    tablaCurso.addEventListener('click', eliminarCurso);
+
+    //evento para ejecutar vaciar carrito
+    btnVaciarCarrito.addEventListener('click', vaciarCarrito);
 });
--- practica_javascript/clase-3/practica_carrito_cursos/src/components/carrito.js
+++ practica_javascript/clase-3/practica_carrito_cursos/src/components/carrito.js
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 //declarando el arreglo para guardar los cursos
 let arregloCarrito = [];
 //
@@ -11,6 +13,18 @@ export function agregarCurso(e) {
         const cursoSeleccionado = e.target.parentElement.parentElement;
         /* console.log(cursoSeleccionado); */
 
+        //una vez seleccionado el curso vamos a deshabilitar el boton
+        e.target.classList.add('disabled');
+        //cambiando el texto del boton
+        e.target.textContent = 'Ir al carrito';
+
+        //asignamos una alerta
+        Swal.fire({
+            icon: "success",
+            title: "Agregaste un curso",
+            showConfirmButton: false,
+            timer: 1500
+        });
         //llamamos la funcion para leer los datos del curso
         leerDatosCurso(cursoSeleccionado);
     }
@@ -31,14 +45,27 @@ function leerDatosCurso(curso) {
     //agregar elementos al arreglo
     arregloCarrito.push(objetoCurso);
 
-    console.log(arregloCarrito);
+    /* console.log(arregloCarrito); */
 
     //Dibujamos la el tbody con los datos
     carritoHTML();
 }
 
+//funcio calcular el valor total del carrito
+function valorCarrito() {
+    let total = 0;
+
+    arregloCarrito.map(curso => {
+        total += parseFloat(curso.precio.replace('$', ''));
+    });
+
+    return total;
+}
+
 //Iterar el arreglo de los cursos en la tabla html
 function carritoHTML() {
+    //resetear el tbody
+    bodyCarrito.innerHTML = '';
     arregloCarrito.map(curso => {
         //creando una fila
         const fila = document.createElement('tr');
@@ -60,5 +87,101 @@ function carritoHTML() {
 
         //insertando las filas en el tbody
         bodyCarrito.appendChild(fila);
+    });
+
+    const filaTotal = document.createElement('tr');
+    filaTotal.innerHTML = `
+        <td colspan="2"></td>
+        <td><strong>Total:</strong></td>
+        <td>$${valorCarrito().toFixed(2)}</td>
+    `;
+
+    bodyCarrito.appendChild(filaTotal);
+}
+
+//funcion para eliminar un curso por ID
+export function eliminarCurso(e) {
+    if(e.target.classList.contains('borrar_curso')) {
+        //obtenemos el data id del curso que selecciono la persona
+        const cursoId = e.target.getAttribute('data-id');
+
+        //boton que voy habilitar si se elimina el curso
+        const habilitarBoton = document.querySelector(`button[data-id="${cursoId}"]`);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //filtrar los cursos que sean diferente al id seleccionado
+                arregloCarrito = arregloCarrito.filter(curso => curso.id !== cursoId);
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+                
+                //condicionamos para habilitar el boton
+                if(habilitarBoton) {
+                    //removemos la clase disable
+                    habilitarBoton.classList.remove('disabled');
+
+                    //Cambiamos el texto
+                    habilitarBoton.textContent = 'Agregar a carrito';
+                }
+
+                //Una vez filtrado el arreglo, actualizamos la tabla
+                carritoHTML();
+            }
+        });
+    }
+}
+
+//funcion para vaciar el carrito
+export function vaciarCarrito() {
+    Swal.fire({
+        title: "¿Estás seguro de que deseas vaciar el carrito?",
+        text: "Esta acción no se puede revertir",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, Eliminar!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            //recorrer el arreglo para habilitar botones
+            arregloCarrito.map(curso => {
+                const cursoID = curso.id;
+        
+                //boton que voy habilitar si se elimina el curso
+                const habilitarBoton = document.querySelector(`button[data-id="${cursoID}"]`);
+        
+                //condicionamos para habilitar el boton
+                if(habilitarBoton) {
+                    //removemos la clase disable
+                    habilitarBoton.classList.remove('disabled');
+        
+                    //Cambiamos el texto
+                    habilitarBoton.textContent = 'Agregar a carrito';
+                }
+            });
+        
+            Swal.fire({
+                title: "Eliminado!",
+                text: "Tu carrito ha sido eliminado.",
+                icon: "success"
+            });
+            
+            //vaciar el arreglo carrito
+            arregloCarrito = [];
+
+            //mostrar la tabla carrito vacia
+            carritoHTML();
+        }
     });
 }
